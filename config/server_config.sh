ac2a93bb49e9e001d5d36939d58d803fe034f586
bwalwyn <bwalwyn@gmail.com>
2019-08-05 21:36:29 +0200

    fix server config

--- config/server_config.sh
+++ config/server_config.sh
@@ -1,23 +1,20 @@
 ### Config
-rm -r yellow
-
-git clone
 
 # delete crontab
 crontab -r
 
 # add new crontab 
-crontab tools/cron_setup.txt
+crontab config/cron_setup.txt
 
 # list crontab to check
 crontab -l
 
 # Copy the .bash_profile
-cp .bash_profile ~/.bash_profile  
+cp config/.bash_profile ~/.bash_profile  
 
 # Update venv for data-jobs
-source .venv/data-jobs/bin/activate
-pip install -r requirements
+source ~/.venv/data-jobs/bin/activate
+pip install -r requirements.txt
 deactivate
 
 
